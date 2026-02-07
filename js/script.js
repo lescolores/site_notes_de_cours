25aabd91c088a534d2d981ce5f4f916c457fc11a
lescolores <info@lescolores.ca>
2026-02-07 15:13:46 -0500

    Modifs 7 février

---
+++ js/script.js
@@ -0,0 +1,29 @@
+window.onload = function() {
+    // Accueil de PHP
+    const mesPages = [ // Nom de chaque page
+        { texte: "Symboles", fichier: "index.html" },
+        { texte: "Conditions if else", fichier: "conditionnelles.html" },
+        { texte: "Boucles", fichier: "boucles.html" }
+    ]; 
+
+    const menu = document.getElementById('menu-sidebar');
+
+    if (menu) {
+        mesPages.forEach(page => {
+            const lien = document.createElement('a');
+
+            lien.href = page.fichier; // Le fichier servira de destination
+
+            lien.textContent = page.texte; // Le texte servira d'affichage
+
+            if (window.location.href.includes(page.fichier)) { // Permet de mettre une class pour la page active
+                lien.classList.add('page-active');
+            }
+
+            menu.appendChild(lien);
+        });
+    }
+
+    // Footer
+    document.getElementById("year").textContent = new Date().getFullYear();
+};
